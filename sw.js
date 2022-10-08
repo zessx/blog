6867c061977fef1435a162ea23d98faf5f41438d
Samuel Marchal <contact@smarchal.com>
2022-10-08 18:30:04 +0200

    :zap: Stale-while-revalidate cache policy

--- sw.js
+++ sw.js
@@ -1,41 +1,77 @@
 // Service Worker
 
-importScripts('/js/cache-polyfill.js');
+importScripts('/js/cache-polyfill.js')
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('blog-smarchal').then(function(cache) {
+const CACHE_NAME = 'blog-smarchal'
+var CACHED_URLS = [
+  '/',
+  '/css/main.css',
+  '/js/app.js',
+  '/js/jekyll-search.min.js',
+  '/images/logo.png',
+  '/fonts/sourcecodepro-light-webfont.woff',
+  '/fonts/SourceSansPro-ExtraLight.woff',
+  '/search.json',
+  '/manifest.json'
+]
 
-      var urls = [
-        '/',
-        '/css/main.css',
-        '/js/app.js',
-        '/js/jekyll-search.min.js',
-        '/images/logo.png',
-        '/fonts/sourcecodepro-light-webfont.woff',
-        '/fonts/SourceSansPro-ExtraLight.woff',
-        '/search.json',
-        '/manifest.json',
-      ];
-
+// Open cache on install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
       const request = async () => {
         var response = await fetch('/search.json')
-        var posts = await response.json();
+        var posts = await response.json()
         for (var post in posts) {
-          urls.push(post.url);
+          CACHED_URLS.push(post.url)
         }
       }
-      request();
+      request()
 
-      return cache.addAll(urls);
+      return cache.addAll(CACHED_URLS)
     })
-  );
-});
+  )
+})
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
-});
+// Cache and update with stale-while-revalidate policy
+self.addEventListener('fetch', event => {
+  const { request } = event
+
+  // Prevent Chrome Developer Tools error:
+  // Failed to execute 'fetch' on 'ServiceWorkerGlobalScope': 'only-if-cached' can be set only with 'same-origin' mode
+  if (request.cache === 'only-if-cached' && request.mode !== 'same-origin') {
+    return
+  }
+
+  event.respondWith(async function () {
+    const cache = await caches.open(CACHE_NAME)
+
+    const cachedResponsePromise = await cache.match(request)
+    const networkResponsePromise = fetch(request)
+
+    if (request.url.startsWith(self.location.origin)) {
+      event.waitUntil(async function () {
+        const networkResponse = await networkResponsePromise
+
+        await cache.put(request, networkResponse.clone())
+      }())
+    }
+
+    return cachedResponsePromise || networkResponsePromise
+  }())
+})
+
+// Clean up caches other than current.
+self.addEventListener('activate', event => {
+  event.waitUntil(async function () {
+    const cacheNames = await caches.keys()
+
+    await Promise.all(
+      cacheNames.filter(cacheName => {
+        const deleteThisCache = cacheName !== CACHE_NAME
+
+        return deleteThisCache
+      }).map(cacheName => caches.delete(cacheName))
+    )
+  }())
+})
