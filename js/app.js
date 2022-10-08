9097fa9bc789759e40a077316c0eee933359dbc6
Samuel Marchal <contact@smarchal.com>
2022-10-08 18:26:29 +0200

    :lipstick: Change trianglification

--- js/app.js
+++ js/app.js
@@ -1,145 +1,124 @@
+// Debouncer
+function debounce(funct, timeout) {
+   var timeoutID , timeout = timeout || 200;
+   return function () {
+      var scope = this , args = arguments;
+      clearTimeout( timeoutID );
+      timeoutID = setTimeout( function () {
+          funct.apply( scope , Array.prototype.slice.call( args ) );
+      } , timeout );
+   }
+}
+
+// setAttribute multiple
+Element.prototype.setAttributes = function(attrs) {
+  for(var key in attrs) {
+    this.setAttribute(key, attrs[key]);
+  }
+}
+
 // Trianglified header
-function drawHeader() {
-  var titleHeightFull = document.querySelector('#title').clientHeight;
-  var refreshDuration = 10000;
-  var refreshTimeout;
-  var numPointsX;
-  var numPointsY;
-  var unitWidth;
-  var unitHeight;
-  var points;
-
-  var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  svg.setAttribute('width',window.innerWidth);
-  svg.setAttribute('height',titleHeightFull);
-  document.querySelector('#bg').appendChild(svg);
-
-  var unitSize = (window.innerWidth+titleHeightFull)/20;
-  numPointsX = Math.ceil(window.innerWidth/unitSize)+1;
-  numPointsY = Math.ceil(titleHeightFull/unitSize)+1;
-  unitWidth = Math.ceil(window.innerWidth/(numPointsX-1));
-  unitHeight = Math.ceil(titleHeightFull/(numPointsY-1));
+var points = [];
+var polygons = [];
+function trianglifyLimit() {
+    limit = window.innerHeight - window.pageYOffset;
+    polygons.forEach(function(polygon, index) {
+        if(window.pageYOffset == 0 || Math.max.apply(null, polygon.getAttribute('points').replace(new RegExp(/[\d\.]+\,/g), '').split(' ').map(function(element) { return parseFloat(element); })) < limit) {
+            polygon.classList.add('visible');
+        } else {
+            polygon.classList.remove('visible');
+        }
+    });
+}
+function trianglify() {
+  var svg = document.querySelector('#canvas'),
+      svgBackground = document.querySelector('#canvas-background'),
+      svgPolygons = document.querySelector('#canvas-polygons'),
+      svgMask = document.querySelector('#canvas-mask');
 
   points = [];
+  polygons = [];
+  while (svgPolygons.firstChild) {
+      svgPolygons.removeChild(svgPolygons.firstChild);
+  }
+  while (svgMask.firstChild) {
+      svgMask.removeChild(svgMask.firstChild);
+  }
+
+  var margin = 30,
+      fullWidth = window.innerWidth - margin * 2,
+      fullHeight = window.innerHeight - margin * 2,
+      attributes = {
+        'width': fullWidth,
+        'height': fullHeight
+      }
+
+  svg.setAttributes(attributes);
+  svgPolygons.setAttributes(attributes);
+  svgBackground.setAttributes(attributes);
+
+  var unitSize = (fullWidth+fullHeight)/20;
+      numPointsX = Math.ceil(fullWidth/unitSize)+1;
+      numPointsY = Math.ceil(fullHeight/unitSize)+1;
+      unitWidth = Math.ceil(fullWidth/(numPointsX-1));
+      unitHeight = Math.ceil(fullHeight/(numPointsY-1));
 
   for(var y = 0; y < numPointsY; y++) {
       for(var x = 0; x < numPointsX; x++) {
-          points.push({x:unitWidth*x, y:unitHeight*y, originX:unitWidth*x, originY:unitHeight*y});
+          points.push([
+              unitWidth * x + ((x == 0 || x == numPointsX) ? 0 : (Math.random() * unitWidth - unitWidth / 2) / 1.4),
+              unitHeight * y + ((y == 0 || y == numPointsY) ? 0 : (Math.random() * unitHeight - unitHeight / 2) / 1.4)
+          ]);
       }
   }
 
-  randomizeHeader();
-
   for(var i = 0; i < points.length; i++) {
-      if(points[i].originX != unitWidth*(numPointsX-1) && points[i].originY != unitHeight*(numPointsY-1)) {
-          var topLeftX = points[i].x;
-          var topLeftY = points[i].y;
-          var topRightX = points[i+1].x;
-          var topRightY = points[i+1].y;
-          var bottomLeftX = points[i+numPointsX].x;
-          var bottomLeftY = points[i+numPointsX].y;
-          var bottomRightX = points[i+numPointsX+1].x;
-          var bottomRightY = points[i+numPointsX+1].y;
-
+      if(i % numPointsX != numPointsX - 1 && i <= numPointsY * numPointsX - numPointsX - 1) {
           var rando = Math.floor(Math.random()*2);
-
           for(var n = 0; n < 2; n++) {
-              var polygon = document.createElementNS(svg.namespaceURI, 'polygon');
-
+              var polygon = document.createElementNS(svg.namespaceURI, 'polygon'),
+                  coords = '';
               if(rando==0) {
                   if(n==0) {
-                      polygon.point1 = i;
-                      polygon.point2 = i+numPointsX;
-                      polygon.point3 = i+numPointsX+1;
-                      polygon.setAttribute('points',topLeftX+','+topLeftY+' '+bottomLeftX+','+bottomLeftY+' '+bottomRightX+','+bottomRightY);
+                      coords = points[i].join(',')+' '+points[i+numPointsX].join(',')+' '+points[i+numPointsX+1].join(',');
                   } else if(n==1) {
-                      polygon.point1 = i;
-                      polygon.point2 = i+1;
-                      polygon.point3 = i+numPointsX+1;
-                      polygon.setAttribute('points',topLeftX+','+topLeftY+' '+topRightX+','+topRightY+' '+bottomRightX+','+bottomRightY);
+                      coords = points[i].join(',')+' '+points[i+1].join(',')+' '+points[i+numPointsX+1].join(',');
                   }
               } else if(rando==1) {
                   if(n==0) {
-                      polygon.point1 = i;
-                      polygon.point2 = i+numPointsX;
-                      polygon.point3 = i+1;
-                      polygon.setAttribute('points',topLeftX+','+topLeftY+' '+bottomLeftX+','+bottomLeftY+' '+topRightX+','+topRightY);
+                      coords = points[i].join(',')+' '+points[i+numPointsX].join(',')+' '+points[i+1].join(',');
                   } else if(n==1) {
-                      polygon.point1 = i+numPointsX;
-                      polygon.point2 = i+1;
-                      polygon.point3 = i+numPointsX+1;
-                      polygon.setAttribute('points',bottomLeftX+','+bottomLeftY+' '+topRightX+','+topRightY+' '+bottomRightX+','+bottomRightY);
+                      coords = points[i+numPointsX].join(',')+' '+points[i+1].join(',')+' '+points[i+numPointsX+1].join(',');
                   }
               }
-              polygon.setAttribute('fill','rgba(0,0,0,'+(Math.random()/6)+')');
-              var animate = document.createElementNS('http://www.w3.org/2000/svg','animate');
-              animate.setAttribute('fill','freeze');
-              animate.setAttribute('attributeName','points');
-              animate.setAttribute('dur',refreshDuration+'ms');
-              animate.setAttribute('calcMode','linear');
-              polygon.appendChild(animate);
-              svg.appendChild(polygon);
+              polygon.setAttributes({
+                'points': coords,
+                'fill': 'rgba(0,0,0,'+(Math.random()/4)+')'
+              });
+              polygonClone = polygon.cloneNode();
+              polygons.push(polygon);
+              polygons.push(polygonClone);
+              svgPolygons.appendChild(polygonClone);
+              svgMask.appendChild(polygon);
           }
       }
   }
 
-  refreshHeader();
-
-  function randomizeHeader() {
-      for(var i = 0; i < points.length; i++) {
-          if(points[i].originX != 0 && points[i].originX != unitWidth*(numPointsX-1)) {
-              points[i].x = points[i].originX + Math.random()*unitWidth-unitWidth/2;
-          }
-          if(points[i].originY != 0 && points[i].originY != unitHeight*(numPointsY-1)) {
-              points[i].y = points[i].originY + Math.random()*unitHeight-unitHeight/2;
-          }
-      }
-  }
-
-  function refreshHeader() {
-      clearTimeout(refreshTimeout);
-      randomizeHeader();
-      for(var i = 0; i < document.querySelector('#bg svg').childNodes.length; i++) {
-          var polygon = document.querySelector('#bg svg').childNodes[i];
-          var animate = polygon.childNodes[0];
-          if(animate.getAttribute('to')) {
-              animate.setAttribute('from',animate.getAttribute('to'));
-          }
-          animate.setAttribute('to',points[polygon.point1].x+','+points[polygon.point1].y+' '+points[polygon.point2].x+','+points[polygon.point2].y+' '+points[polygon.point3].x+','+points[polygon.point3].y);
-          animate.beginElement();
-      }
-      refreshTimeout = setTimeout(function() {refreshHeader();}, refreshDuration);
-  }
+  trianglifyLimit();
+  document.addEventListener('scroll', trianglifyLimit);
 }
-window.addEventListener('DOMContentLoaded', drawHeader, false);
-
-// Sticky header
-function stickyHeaderInit() {
-  Array.prototype.forEach.call(document.querySelectorAll('article.full-article, section.articles, section.tags'), function(el, i){
-    el.style.paddingTop = (document.querySelector('header').clientHeight + 50) + 'px';
-  });
-  stickyHeader();
-}
-function stickyHeader() {
-  var header = document.querySelector('header'),
-      sticked = header.classList.contains('sticky');
-  if(window.scrollY > 0 && !sticked) {
-    header.classList.add('sticky');
-  }
-  if(window.scrollY < 80 && sticked) {
-    header.classList.remove('sticky');
-  }
-}
-window.addEventListener('scroll', stickyHeader, false);
-document.addEventListener('DOMContentLoaded', stickyHeaderInit, false);
+document.addEventListener('DOMContentLoaded', trianglify);
+window.addEventListener('resize', debounce(trianglify, 50));
 
 // Headings' anchors
 function headingAnchors() {
   var headings = document.querySelectorAll('h2[id], h3[id], h4[id]');
   Array.prototype.forEach.call(headings, function(el, i){
     var link = document.createElement('a');
-    link.setAttribute('name', el.getAttribute('id'));
-    link.setAttribute('href', '#' + el.getAttribute('id'));
+    link.setAttributes({
+      'name': el.getAttribute('id'),
+      'href': '#' + el.getAttribute('id')
+    });
     el.removeAttribute('id');
     el.appendChild(link);
   });
@@ -257,6 +236,7 @@ function infiniteLoad() {
               post.setAttribute('data-color', tag);
             }
           }
+          post.classList.remove('full-article');
           document.querySelector('.articles').appendChild(post);
         });
         callback();
